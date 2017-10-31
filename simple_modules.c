a39b6781e842fd8ffae5aa59a5245fde10549544
Kezzia Carter <mvandertramps@gmail.com>
2017-10-30 22:14:52 -0400

    Giving this module a different name

--- simple_modules.c
+++ simple_modules.c
@@ -1,17 +1,18 @@
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
+#include <linux/init.h>		/* Needed for the macros */
 
-int init_module(void)
+static int __init pid_info(void)
 {
   printk(KERN_INFO "Hello world 1.\n");
   printk(KERN_INFO "pid\n");
   printk(KERN_INFO "uid, gid of process\n");
   printk(KERN_INFO "all of the children processes pid and name\n");
   printk(KERN_INFO "any flags that are set translated to readable names\n");
-  return 0;
+	return 0;
 }
 
-void cleanup_module(void)
+static void __exit pid_info_exit(void)
 {
-	printk(KERN_INFO "Goodbye world 1.\n");
+  printk(KERN_INFO "Goodbye, world 2\n");
 }
