e8ccd84537a9a9b5b1ae610e503f0b2d78fdf458
Kezzia Carter <mvandertramps@gmail.com>
2017-11-05 11:16:04 -0500

    allowing pid_info to be passed a pid value, setting up some pointers

--- simple_modules.c
+++ simple_modules.c
@@ -1,14 +1,20 @@
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/init.h>		/* Needed for the macros */
+#include <asm/current.h>       // process information, just for fun
+#include <linux/sched.h>       // for "struct task_struct"
 
-int pid = 0;
+#define DRIVER_AUTHOR "Alston, Cesa & Kezzia"
+#define DRIVER_DESC "This module prints the pid, uid, gid, the parent process' pid and name, all children processes' pid and name and flag value set for a given process"
 
-static int __init pid_info(void)
+void pid_info(pid_t pid)
 {
-  *find_get_pid(pid_t nr) /* gets the struct pid of our pid */
-  *pid_task(struct pid *pid, enum pid_type type) /* returns the struct task_struct of our pid */
-  printk(KERN_INFO "PROCESS PID: ");
+  struct task_struct *t_s_pointer;
+  struct task_struct *child;
+  struct task_struct *current;
+  struct task_struct *ch_t;
+
+  printk(KERN_INFO "pid of process\n");
   printk(KERN_INFO "uid, gid of process\n");
   printk(KERN_INFO "all of the children processes pid and name\n");
   printk(KERN_INFO "any flags that are set, translated to readable names\n");
