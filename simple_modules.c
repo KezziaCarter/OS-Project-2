f176ff06f939ba046a000923859251207f70b8cf
Kezzia Carter <mvandertramps@gmail.com>
2017-11-05 13:55:25 -0500

    printing stuff

--- simple_modules.c
+++ simple_modules.c
@@ -1,8 +1,8 @@
-#include <linux/module.h>	/* Needed by all modules */
-#include <linux/kernel.h>	/* Needed for KERN_INFO */
-#include <linux/init.h>		/* Needed for the macros */
-#include <asm/current.h>       // process information, just for fun
-#include <linux/sched.h>       // for "struct task_struct"
+#include <linux/module.h>	  // Needed by all modules
+#include <linux/kernel.h>	  // Needed for KERN_INFO
+#include <linux/init.h>     // Needed for the macros
+#include <asm/current.h>    // Process information
+#include <linux/sched.h>    // For "struct task_struct"
 
 #define DRIVER_AUTHOR "Alston, Cesa & Kezzia"
 #define DRIVER_DESC "This module prints the pid, uid, gid, the parent process' pid and name, all children processes' pid and name and flag value set for a given process"
@@ -14,8 +14,12 @@ void pid_info(pid_t pid)
   struct task_struct *current;
   struct task_struct *ch_t;
 
-  printk(KERN_INFO "pid of process\n");
-  printk(KERN_INFO "uid, gid of process\n");
+  t_s_pointer = pid_task(find_vpid(pid), PIDTYPE_PID);
+  child = t_s_pointer;
+
+  printk(KERN_INFO "PID of process: %i\n", t_s_pointer->pid);
+  printk(KERN_INFO "UID: %i\n", (t_s_pointer->cred)->uid.val);
+  printk(KERN_INFO "GID: %i\n", (point->cred)->gid.val);
   printk(KERN_INFO "all of the children processes pid and name\n");
   printk(KERN_INFO "any flags that are set, translated to readable names\n");
 	return 0;
