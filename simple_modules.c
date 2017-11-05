9b0e29ce60332676d818ef1c828e79a285d40287
Kezzia Carter <mvandertramps@gmail.com>
2017-11-05 14:19:51 -0500

    Printing children, fixing up module init and exit

--- simple_modules.c
+++ simple_modules.c
@@ -1,8 +1,16 @@
-#include <linux/module.h>	  // Needed by all modules
-#include <linux/kernel.h>	  // Needed for KERN_INFO
-#include <linux/init.h>     // Needed for the macros
-#include <asm/current.h>    // Process information
-#include <linux/sched.h>    // For "struct task_struct"
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/pid.h>
+#include <linux/proc_fs.h>
+#include <linux/moduleparam.h>
+#include <asm/current.h>
+#include <linux/sched.h>
+#include <asm/uaccess.h>
+#include <asm/io.h>
+#include <linux/seq_file.h>
+#include <linux/fs.h>
+#include <linux/mm.h>
 
 #define DRIVER_AUTHOR "Alston, Cesa & Kezzia"
 #define DRIVER_DESC "This module prints the pid, uid, gid, the parent process' pid and name, all children processes' pid and name and flag value set for a given process"
@@ -13,7 +21,7 @@ void pid_info(pid_t pid)
   struct task_struct *child;
   struct task_struct *current;
   struct task_struct *ch_t;
-
+  struct list_head list;
   t_s_pointer = pid_task(find_vpid(pid), PIDTYPE_PID);
   child = t_s_pointer;
 
@@ -23,25 +31,31 @@ void pid_info(pid_t pid)
   printk(KERN_INFO "Parent process: %i\n", t_s_pointer->parent->comm);
   printk(KERN_INFO "Parent PID: %i\n", (t_s_pointer->cred)->uid.val);
   printk(KERN_INFO "all of the children processes pid and name\n");
+
+  current = t_s_pointer;
+  list = current->children
+  ch_t = list_entry(&list, struct task_struct, children);
+  printk(KERN_INFO "CHILDREN:\n");
+  printk(KERN_INFO "Child process: %i\n", ch_t->comm);
+  printk(KERN_INFO "Child PID: %i\n", curr->pid);
+  pid_vminfo();
+
   printk(KERN_INFO "any flags that are set, translated to readable names\n");
 	return 0;
 }
 
-static void __exit pid_info_exit(void)
+
+static int pid_info_begin(void)
+{
+  pid_t pid;
+  pid = task_pid_nr(current);
+  pid_info(pid);
+}
+
+static void pid_info_exit(void)
 {
   printk(KERN_INFO "Goodbye, world\n");
 }
 
-/*
- * module_param(foo, int, 0000)
- * The first param is the parameters name
- * The second param is it's data type
- * The final argument is the permissions bits,
- * for exposing parameters in sysfs (if non-zero) at a later stage.
- */
-
-module_param(pid, int, 0);
-MODULE_PARM_DESC(pid, "The pid of the process whose information we're printing\n");
-
-module_init(pid_info);
+module_init(pid_info_begin);
 module_exit(pid_info_exit);
